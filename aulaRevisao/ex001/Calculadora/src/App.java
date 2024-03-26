e8538ba8bae57ee1fab2e54394b0cd989a835465
phbrugnolo <phbrugnolo@gmail.com>
2024-03-26 17:51:17 -0300

    Arruamando a verificação dos numeros na calculadora

--- aulaRevisao/ex001/Calculadora/src/App.java
+++ aulaRevisao/ex001/Calculadora/src/App.java
@@ -1,14 +1,16 @@
 public class App {
     public static void main(String[] args) throws Exception {
 
-        Calculadora conta1 = new Calculadora(5, 2);
+        Calculadora calc1 = new Calculadora(3, 2);
+        calc1.verificaNum();
 
-        System.out.println(conta1.adicao());
-        System.out.println(conta1.subtracao());
-        System.out.println(conta1.multiplicacao());
-        System.out.println(conta1.divisao());
-        System.out.println(conta1.potenciacao());
-        System.out.println(conta1.fatoracao());
+
+        System.out.println(calc1.adicao());
+        System.out.println(calc1.subtracao());
+        System.out.println(calc1.multiplicacao());
+        System.out.println(calc1.divisao());
+        System.out.println(calc1.potenciacao());
+        System.out.println(calc1.fatoracao());
     
     }
 }
--- aulaRevisao/ex001/Calculadora/src/Calculadora.java
+++ aulaRevisao/ex001/Calculadora/src/Calculadora.java
@@ -1,57 +1,49 @@
 public class Calculadora {
 
-
     private float numero1;
     private float numero2;
 
-    
-
-    
     public Calculadora(float numero1, float numero2) {
-        
-        if(numero1 < 0 && numero2 < 0 && numero1 > 50 && numero2 > 50){
-            System.err.println("Os numeros não podem serem maiores que 50 e menores que 0");
-        }else {
-            this.numero1 = numero1;
-            this.numero2 = numero2;
-        }
-        
-        
+        this.numero1 = numero1;
+        this.numero2 = numero2;
     }
-    
+
+    public void verificaNum(){
+        if(numero1 < 0 && numero2 < 0 && numero1 > 50 && numero2 > 50){
+            System.out.println("Os números devem ser maior que 0 e menor que 50");
+        }else {
+            System.out.println("Estes números são aceitos");
+        }
+    }
+
     public float getNumero1() {
         return numero1;
     }
-    
 
     public void setNumero1(float numero1) {
         this.numero1 = numero1;
     }
 
-    
-    
-    
     public float getNumero2() {
         return numero2;
     }
-    
-    
-    
-    
+
     public void setNumero2(float numero2) {
         this.numero2 = numero2;
     }
 
-    
     public float adicao(){
         return numero1 + numero2;
     }
+
     public float subtracao(){
         return numero1 - numero2;
     }
+
     public float multiplicacao(){
         return numero1 * numero2;
     }
+
     public float divisao(){
         return numero1 / numero2;
     }
@@ -72,6 +64,4 @@ public class Calculadora {
         return pot;
     }
 
-
-    
 }
